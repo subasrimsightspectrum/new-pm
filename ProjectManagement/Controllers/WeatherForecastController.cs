54df0478c88e6c1bb2e6cc925c8f74fbb52359ec
w3schools-test <subasrim@sightspectrum.com>
2023-03-23 14:47:00 +0530

    remove properties in weatherForeCatController

--- ProjectManagement/Controllers/WeatherForecastController.cs
+++ ProjectManagement/Controllers/WeatherForecastController.cs
@@ -34,6 +34,5 @@ namespace ProjectManagement.Controllers
         public string Result { get; set; }
         public string Result1 { get; set; }
         public string Result2 { get; set; }
-        public string Result3 { get; set; }
     }
 }
